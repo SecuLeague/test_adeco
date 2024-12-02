2093050852003709c0e60a8a6f74e63433452fd3
SecuLeague <101701884+SecuLeague@users.noreply.github.com>
2024-12-02 10:34:49 +0100

    Update Firewall-01_selenuim.py

--- Intégration des services/Firewall-01_selenuim.py
+++ Intégration des services/Firewall-01_selenuim.py
@@ -4,32 +4,38 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
+import os
 
 try:
     # Configuration des options Chrome
     options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--remote-debugging-port=9222')
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
     
-    # Configuration du service avec installation automatique du driver
+    # Configuration du service avec installation automatique
     service = Service(ChromeDriverManager().install())
     
     # Création du driver
     driver = webdriver.Chrome(service=service, options=options)
+    driver.set_page_load_timeout(20)
     
     # Navigation vers pfSense
     driver.get('http://172.16.150.2')
     
-    # Attente des éléments avec les bons sélecteurs visibles dans l'image
+    # Attente des éléments avec les bons sélecteurs
     wait = WebDriverWait(driver, 10)
-    username = wait.until(EC.presence_of_element_located((By.NAME, "Username")))
-    password = driver.find_element(By.NAME, "Password")
+    username = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[placeholder='Username']")))
+    password = driver.find_element(By.CSS_SELECTOR, "input[placeholder='Password']")
     
     # Remplir les champs
     username.send_keys("admin")
     password.send_keys("pfsense")
     
-    # Cliquer sur le bouton vert SIGN IN
+    # Cliquer sur le bouton SIGN IN
     sign_in = driver.find_element(By.CSS_SELECTOR, "button.btn")
     sign_in.click()
     
