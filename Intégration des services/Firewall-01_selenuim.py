d6968fee1b9a7e197f1a98d1c8cac7743f554060
SecuLeague <101701884+SecuLeague@users.noreply.github.com>
2024-12-02 10:00:07 +0100

    Update Firewall-01_selenuim.py

--- Intégration des services/Firewall-01_selenuim.py
+++ Intégration des services/Firewall-01_selenuim.py
@@ -6,15 +6,16 @@ from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
 try:
-    # Installation et configuration du ChromeDriver
-    service = Service(ChromeDriverManager(version="latest").install())
+    # Installation et configuration du ChromeDriver sans spécifier de version
+    service = Service(ChromeDriverManager().install())
     
     # Configuration des options Chrome
     options = webdriver.ChromeOptions()
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--ignore-certificate-errors')
     
-    # Création du driver avec le service configuré
+    # Création du driver
     driver = webdriver.Chrome(service=service, options=options)
     driver.set_page_load_timeout(300)
     
@@ -23,14 +24,14 @@ try:
     
     # Attente des éléments
     wait = WebDriverWait(driver, 30)
-    username = wait.until(EC.presence_of_element_located((By.NAME, "usernamefld")))
-    password = driver.find_element(By.NAME, "passwordfld")
+    username = wait.until(EC.presence_of_element_located((By.ID, "usernamefld")))
+    password = driver.find_element(By.ID, "passwordfld")
     
     # Remplir les champs
     username.send_keys("admin")
     password.send_keys("pfsense")
     
-    # Cliquer sur le bouton
+    # Cliquer sur le bouton de connexion
     login_button = driver.find_element(By.CLASS_NAME, "btn")
     login_button.click()
     
