dca0dd185b08c840446b45e21406a32e4582c0a1
SecuLeague <101701884+SecuLeague@users.noreply.github.com>
2024-12-02 08:54:46 +0100

    Update Firewall-01_selenuim.py

--- Intégration des services/Firewall-01_selenuim.py
+++ Intégration des services/Firewall-01_selenuim.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 try:
     # Configuration des options Chrome
@@ -9,19 +11,22 @@ try:
     options.add_argument('--headless')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-gpu')
-    options.add_argument('--page-load-strategy=normal')
+    options.add_argument('--disable-software-rasterizer')
     options.add_argument('--window-size=1920,1080')
+    options.add_argument('--start-maximized')
     options.page_load_strategy = 'eager'
-
+    
     # Configuration du service avec timeout augmenté
     service = Service(ChromeDriverManager().install())
+    service.start()
+    
+    # Initialisation du driver avec des timeouts plus longs
     driver = webdriver.Chrome(service=service, options=options)
+    driver.set_page_load_timeout(60)
+    driver.implicitly_wait(30)
     
-    # Augmentation des timeouts
-    driver.set_page_load_timeout(30)
-    driver.implicitly_wait(20)
-    
-    # Navigation vers l'URL
+    # Navigation avec wait explicite
+    wait = WebDriverWait(driver, 30)
     driver.get('http://172.16.100.1:4444')
     
     print("Navigation réussie")
